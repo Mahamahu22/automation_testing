2277359608800c453e9134cea8eaa2a0942ba3e2
Mahamahu22 <mahalakshmirajan07@gmail.com>
2025-10-27 21:14:04 +0530

    updated

--- day8/playwright.config.ts
+++ day8/playwright.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from '@playwright/test';
 import path from 'path';
 
 export default defineConfig({
-  testDir: './tests',     // your test folder
+  testDir: './tests',   
   timeout: 30000,
   use: {
-    headless: false,      // show the browser
-    baseURL: `file://${path.join(__dirname, 'public')}/`,  // local folder for HTML
+    headless: false,     
+    baseURL: `file://${path.join(__dirname, 'public')}/`,  
   },
 });
